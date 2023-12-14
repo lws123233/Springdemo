f8f03671f341bb0b25121fddfad754cbb959906d
lws <1771535975@qq.com>
2023-12-14 10:36:42 +0800

    函数式接口

--- src/main/java/com/example/springdemo/controller/FunctionTest.java
+++ src/main/java/com/example/springdemo/controller/FunctionTest.java
@@ -2,17 +2,36 @@ package com.example.springdemo.controller;
 
 import com.example.springdemo.pojo.Data;
 
-
+/**
+ * 函数式接口测试，functionTest1，functionTest1相当于FunctionTest的两个实现。
+ * 这种写法可以不用写实现类而进行接口的实现
+ * 注意这种接口只能有一个抽象方法
+ */
 public interface FunctionTest {
     FunctionTest functionTest1=(var)->{
-       return null!=var?new Data(var,var,var,var):new Data();
+        System.out.println("这是函数1");
     };
 
     FunctionTest functionTest2=(var)->{
         System.out.println("这是函数2");
-        return new Data();
     };
 
-    Data create(String id);
+
+    FunctionTest choose=(var)->{
+        switch (var){
+            case "functionTest1" : functionTest1.create(var);break;
+            case "functionTest2" : functionTest2.create(var);break;
+            case "functionTest3" : functionTest3(var);break;
+            default: System.out.println("未知函数");;
+        }
+    };
+
+    void create(String id);
+
+
+    static void functionTest3(String var){
+        System.out.println("这是函数3");
+    }
+
 
 }
