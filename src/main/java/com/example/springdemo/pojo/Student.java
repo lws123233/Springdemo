ffb8cab70671c7fe5ed1912c60481498f4c8069b
lws <1771535975@qq.com>
2023-12-14 10:36:29 +0800

    InitializingBean

---
+++ src/main/java/com/example/springdemo/pojo/Student.java
@@ -0,0 +1,39 @@
+package com.example.springdemo.pojo;
+
+import lombok.Data;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Lazy
+@Component
+public class Student implements InitializingBean {
+
+    String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Student(){
+        System.out.println("构造方法调用"+id);
+    }
+    @PostConstruct
+    public void init(){
+        System.out.println("初始化方法调用"+ id);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("属性设置之后方法调用"+id);
+    }
+
+
+}
